b45e887381768e32fc5d16b501bf9852434cdf9c
Your Name <ozkin1vs@hotmail.com>
2023-12-15 14:37:42 -0500

    creando entities

---
+++ src/main/java/com/ozkin/practicamarket/persistence/entity/Compra.java
@@ -0,0 +1,73 @@
+package com.ozkin.practicamarket.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compras")
+    private Integer idCompra;
+
+    @Column(name = "id_cleintes")
+    private String idClientes;
+
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    private String comentario;
+    private String estado;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public String getIdClientes() {
+        return idClientes;
+    }
+
+    public void setIdClientes(String idClientes) {
+        this.idClientes = idClientes;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getMedioPago() {
+        return medioPago;
+    }
+
+    public void setMedioPago(String medioPago) {
+        this.medioPago = medioPago;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
