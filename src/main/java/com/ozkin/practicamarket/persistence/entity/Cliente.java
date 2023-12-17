b9f584abfcc244855358344acb1bf9b5363e08bb
Your Name <ozkin1vs@hotmail.com>
2023-12-16 20:15:00 -0500

    anotaciones ManyToOne and OneToMany y mapeo de relaciones

--- src/main/java/com/ozkin/practicamarket/persistence/entity/Cliente.java
+++ src/main/java/com/ozkin/practicamarket/persistence/entity/Cliente.java
@@ -1,9 +1,8 @@
 package com.ozkin.practicamarket.persistence.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "clientes")
@@ -19,6 +18,9 @@ public class Cliente {
 
     @Column(name = "correo_electronico")
     private String correoElectronico;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
 
     public String getNombre() {
         return nombre;
