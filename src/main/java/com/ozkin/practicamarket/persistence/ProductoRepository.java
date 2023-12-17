4f4614b7aa7c616a9c084aa712c5a3635abf4dd4
Ozkin <ozkin1vs@hotmail.com>
2023-12-17 18:30:47 -0500

    query methods

--- src/main/java/com/ozkin/practicamarket/persistence/ProductoRepository.java
+++ src/main/java/com/ozkin/practicamarket/persistence/ProductoRepository.java
@@ -4,6 +4,7 @@ import com.ozkin.practicamarket.persistence.crud.ProductoCRUDRepository;
 import com.ozkin.practicamarket.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCRUDRepository productoCRUDRepository;
@@ -12,4 +13,10 @@ public class ProductoRepository {
         return (List<Producto>) productoCRUDRepository.findAll();
     }
 
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCRUDRepository.findByIdCategoriaOrderByNameAsc(idCategoria);
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCRUDRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
 }
--- src/main/java/com/ozkin/practicamarket/persistence/crud/ProductoCRUDRepository.java
+++ src/main/java/com/ozkin/practicamarket/persistence/crud/ProductoCRUDRepository.java
@@ -3,5 +3,14 @@ package com.ozkin.practicamarket.persistence.crud;
 import com.ozkin.practicamarket.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCRUDRepository extends CrudRepository<Producto,Integer> {
+   List<Producto> findByIdCategoriaOrderByNameAsc (int idCategoria);
+
+   Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+   
+
+
 }
